8b976e906b629f1021cc7eccecada5022c7346d3
Vipul Vinod Patil <vipulvpatil@gmail.com>
2023-08-23 11:19:45 +0530

    added API code to add/update a candidate

--- internal/server/candidate_test.go
+++ internal/server/candidate_test.go
@@ -205,3 +205,142 @@ func Test_GetCandidates(t *testing.T) {
 		})
 	}
 }
+
+func Test_UpdateCandidate(t *testing.T) {
+	team, _ := model.NewTeam(model.TeamOptions{
+		Id:   "team_id1",
+		Name: "test@example.com",
+	})
+	userWithTeam, _ := model.NewUser(model.UserOptions{
+		Id:    "user_id1",
+		Email: "test@example.com",
+		Team:  team,
+	})
+
+	tests := []struct {
+		name                  string
+		ctx                   context.Context
+		input                 *pb.UpdateCandidateRequest
+		output                *pb.UpdateCandidateResponse
+		teamHydratorMock      storage.TeamHydrator
+		candidateAccessorMock storage.CandidateAccessor
+		errorExpected         bool
+		errorString           string
+	}{
+		{
+			name:                  "errors if no user in context",
+			ctx:                   context.Background(),
+			input:                 &pb.UpdateCandidateRequest{},
+			output:                &pb.UpdateCandidateResponse{},
+			teamHydratorMock:      nil,
+			candidateAccessorMock: nil,
+			errorExpected:         true,
+			errorString:           "rpc error: code = Unauthenticated desc = retrieving user data failed",
+		},
+		{
+			name: "errors if unable to hydrate team",
+			ctx: metadata.NewIncomingContext(
+				context.Background(), metadata.New(
+					map[string]string{
+						requestingUserIdCtxKey:    "user_id1",
+						requestingUserEmailCtxKey: "user@example.com",
+					},
+				),
+			),
+			input:                 &pb.UpdateCandidateRequest{},
+			output:                &pb.UpdateCandidateResponse{},
+			teamHydratorMock:      &storage.TeamHydratorMockFailure{},
+			candidateAccessorMock: nil,
+			errorExpected:         true,
+			errorString:           "unable to hydrate team",
+		},
+		{
+			name: "returns error if unable to parse persona",
+			ctx: metadata.NewIncomingContext(
+				context.Background(), metadata.New(
+					map[string]string{
+						requestingUserIdCtxKey:    "user_id1",
+						requestingUserEmailCtxKey: "user@example.com",
+					},
+				),
+			),
+			input:                 &pb.UpdateCandidateRequest{},
+			output:                nil,
+			teamHydratorMock:      &storage.TeamHydratorMockSuccess{User: userWithTeam},
+			candidateAccessorMock: &storage.CandidateAccessorConfigurableMock{},
+			errorExpected:         true,
+			errorString:           "unable to parse persona json: unexpected end of JSON input",
+		},
+		{
+			name: "returns error if database errors when updating candidate",
+			ctx: metadata.NewIncomingContext(
+				context.Background(), metadata.New(
+					map[string]string{
+						requestingUserIdCtxKey:    "user_id1",
+						requestingUserEmailCtxKey: "user@example.com",
+					},
+				),
+			),
+			input: &pb.UpdateCandidateRequest{
+				ManuallyCreatedPersona: "{\"Name\":\"manual persona 1\",\"Email\":\"email_1\",\"Phone\":\"phone_1\",\"City\":\"city_1\",\"State\":\"state_1\",\"Country\":\"country_1\",\"YoE\":5,\"Tech Skills\":[\"tech skill 1\",\"tech skill 2\",\"tech skill 3\"]}",
+			},
+			output:           nil,
+			teamHydratorMock: &storage.TeamHydratorMockSuccess{User: userWithTeam},
+			candidateAccessorMock: &storage.CandidateAccessorConfigurableMock{
+				UpdateCandidateWithManuallyCreatedPersonaForTeamInternal: func(id string, persona *model.Persona, team *model.Team) error {
+					return errors.New("dbError when updating")
+				},
+			},
+			errorExpected: true,
+			errorString:   "dbError when updating",
+		},
+		{
+			name: "runs successfully",
+			ctx: metadata.NewIncomingContext(
+				context.Background(), metadata.New(
+					map[string]string{
+						requestingUserIdCtxKey:    "user_id1",
+						requestingUserEmailCtxKey: "user@example.com",
+					},
+				),
+			),
+			input: &pb.UpdateCandidateRequest{
+				ManuallyCreatedPersona: "{\"Name\":\"manual persona 1\",\"Email\":\"email_1\",\"Phone\":\"phone_1\",\"City\":\"city_1\",\"State\":\"state_1\",\"Country\":\"country_1\",\"YoE\":5,\"Tech Skills\":[\"tech skill 1\",\"tech skill 2\",\"tech skill 3\"]}",
+			},
+			output:           &pb.UpdateCandidateResponse{},
+			teamHydratorMock: &storage.TeamHydratorMockSuccess{User: userWithTeam},
+			candidateAccessorMock: &storage.CandidateAccessorConfigurableMock{
+				UpdateCandidateWithManuallyCreatedPersonaForTeamInternal: func(id string, persona *model.Persona, team *model.Team) error {
+					return nil
+				},
+			},
+			errorExpected: false,
+			errorString:   "",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			server, _ := NewServer(ServerDependencies{
+				Storage: storage.NewStorageAccessorMock(
+					storage.WithTeamHydratorMock(tt.teamHydratorMock),
+					storage.WithCandidateAccessorMock(tt.candidateAccessorMock),
+				),
+				Logger: &utilities.NullLogger{},
+			})
+
+			response, err := server.UpdateCandidate(
+				tt.ctx,
+				tt.input,
+			)
+			if !tt.errorExpected {
+				assert.Empty(t, tt.errorString)
+				assert.NoError(t, err)
+				assert.EqualValues(t, tt.output, response)
+			} else {
+				assert.NotEmpty(t, tt.errorString)
+				assert.EqualError(t, err, tt.errorString)
+			}
+		})
+	}
+}
